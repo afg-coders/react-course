2111345e9ca4d4f156c02b432bd0f86417c78cf4
abbasatayee <abbas.ataie.montazer@gmail.com>
2023-07-29 10:07:17 +0430

    display details page content

--- src/pages/posts/detail.page.js
+++ src/pages/posts/detail.page.js
@@ -1,10 +1,39 @@
+import { useQuery } from "@tanstack/react-query";
 import React from "react";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
+import { getOneTodo } from "../../utils/api";
+import { Button, LoadingOverlay } from "@mantine/core";
 
 function PostDetail() {
-  const { post_id } = useParams();
-  console.log(post_id);
-  return <div>PostDetail</div>;
+  const { id } = useParams();
+  const navigate = useNavigate();
+
+  const { data, isLoading, isError } = useQuery({
+    queryKey: ["fetch-post", id],
+    queryFn: () => getOneTodo(id),
+  });
+
+  if (isLoading) {
+    return <LoadingOverlay visible />;
+  }
+
+  if (isError) {
+    return <h1>Error Occurred!</h1>;
+  }
+  return (
+    <div>
+      <Button
+        variant="outline"
+        onClick={() => {
+          navigate("/");
+        }}
+      >
+        Back
+      </Button>
+      <h1>{data?.title}</h1>
+      <p>{data?.body}</p>
+    </div>
+  );
 }
 
 export default PostDetail;
